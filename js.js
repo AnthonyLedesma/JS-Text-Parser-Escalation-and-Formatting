62ae13773e456d56980e2cdc34412dcc29ee3497
paranoia1906 <anthonymledesma@gmail.com>
2018-06-05 11:06:00 -0700

    Added formatting for default and primary domain names. Resetting variables for the next parse.

--- js.js
+++ js.js
@@ -4,7 +4,7 @@ var RESULT_BOX = document.getElementById('resultBox');
 var PARSE_BUTTON = document.getElementById('parseButton')
 
 var PASTE_BOX_VALUE = PASTE_BOX.innerHTML;
-
+var DEFAULT_OR_PRIMARY = 0; //Index 0 is default domian, Index 1 is Primary Domain. Top level for scope.
 
 //All functionality tied to button click event at this time.
 PARSE_BUTTON.addEventListener("click", function(){
@@ -27,7 +27,8 @@ PARSE_BUTTON.addEventListener("click", function(){
             //console.log('What was matched is : ' + ORIGIN_ARRAY[x] );
             //console.log('The current array value we are pushing to is : ');
             //console.log(RESULT_ARRAY);
-            RESULT_ARRAY.push(ORIGIN_ARRAY[x]);
+            DEFAULT_OR_PRIMARY == 0 ? RESULT_ARRAY.push("Default Domain: " + ORIGIN_ARRAY[x]) : RESULT_ARRAY.push("Primary Domain: " + ORIGIN_ARRAY[x]);
+            DEFAULT_OR_PRIMARY++;//First pass is default and next pass is primary domain.
         }
 
         if (ORIGIN_ARRAY[x].match(/Site ID/i) != null){ //Using Increment to pull adjacent array value.
@@ -36,13 +37,13 @@ PARSE_BUTTON.addEventListener("click", function(){
             RESULT_ARRAY.push(ORIGIN_ARRAY[INDEX]);
         }
 
-        if (ORIGIN_ARRAY[x].match(/^\#(\d+)/) != null) {
+        if (ORIGIN_ARRAY[x].match(/^\#(\d+)/) != null) { //Pull Customer number and remove the #
             let CUSTOMER_NUM = ORIGIN_ARRAY[x];
-            RESULT_ARRAY.push("Customer Number: " + CUSTOMER_NUM.substr(1));
+            RESULT_ARRAY.push("Customer Number: " + CUSTOMER_NUM.substr(1)); //remove #
 
         }
     }
-    
+    DEFAULT_OR_PRIMARY = 0; //Reset to 0 for next toolkit paste.
     for(var x in RESULT_ARRAY) { //Print to the results box. Increment through Result Array.
         console.log(RESULT_ARRAY[x]);
         RESULT_BOX.append(RESULT_ARRAY[x]);
@@ -50,3 +51,17 @@ PARSE_BUTTON.addEventListener("click", function(){
     }
 
 });
+
+
+//Contant Template For Reference 
+//#### MWP 2.0 Assistance Request ####
+
+//Customer Number:
+
+//siteID:
+
+//Default Domain:
+
+//Primary Domain:
+
+//Request:
