99896993db3240884f36e499e515a57f58cd6aa1
paranoia1906 <anthonymledesma@gmail.com>
2018-06-19 11:18:25 -0700

    Added logic to check for 2 domains no more no less. Should prevent bad results in parse.

--- TextParserJS.js
+++ TextParserJS.js
@@ -17,84 +17,147 @@ let sensitiveSlackArray = []; //Escalation array which still contains sensitive 
 //3 top level event listeners [ParseButton, ResetButton, Checkbox Listeners]
 //Parse button click events.
 parseButton.addEventListener("click", function(){
+    if (!SafeToPrintResultsBox()){return;}
     // ** Breaking the regexes out into variables for readability / scalability
     const urlRegex = /^https:\/\/[^/]+/i;
     const customerNumberRegex = /^\#(\d+)/;
+    let urlFound = 0; //Count should max at 2. Blocks parsing output.
 
     //Splitting long string into array by first splitting new lines. 
     originArray = pasteBox.value.split("\n");
     resultArray = []; // Result array will contain escalation details. 
     resultArray.push('#### MWP 2.0 Assistance Request ####\n'); //Start of template
 
-    originArray.forEach(function(element, index) {
+    originArray.forEach(function(element, index, arr) {
        element = element.replace(/^\s+/i, '');
-        
         if(element.match(urlRegex)) {
-            resultArray.push(`${originArray[index - 1]}: ${element}\n`);
-
+            resultArray.push(`${arr[index - 1]}: ${element}\n`);
             // set domain values for site checks later.
-            if (originArray[index - 1] === 'Default Domain') {
+            if (arr[index - 1] === 'Default Domain') {
                 defaultDomainValue = element;
-            } else if (originArray[index - 1] === 'Primary Domain') {
+                urlFound++;
+            } else if (arr[index - 1] === 'Primary Domain') {
                 primaryDomainValue = element;
+                urlFound++;
             }
         }
 
         if (element.match(/Site ID/i)){ //Using Increment to pull adjacent array value.
-            resultArray.push(`Site ID: ${originArray[index + 1]} \n`);
-        }
-
+            resultArray.push(`Site ID: ${arr[index + 1]}\n`);
+            }
+        
         if (element.match(customerNumberRegex)) { //Pull Customer number and remove the #
-        resultArray.push(`Customer Number: ${element.substr(1)} \n`); //remove # character
+        resultArray.push(`Customer Number: ${element.substr(1)}\n`); //remove # character
         }
     });
+    if (urlFound !== 2) {
+        console.log(`Error Found : ${urlFound} URLs - (Should be 2)`)
+        PartialReset();
+        return;
+    }
     resultArray.push(`\nSituation: ${situationBox.value}\n`);//alter value to end of each array itteration.
 
-    if (situationBox.value != '' && pasteBox.value != ''){//Confirm that values exist
-        resultArray.forEach(function (element){ //Print to the results box. Increment through Result Array.
+    if (SafeToPrintResultsBox()){//Checks for empty form fields.
+        resultArray.forEach(function (element){ 
             resultBox.value = resultBox.value + element;//Iterate each key and value
         });
-        resultBox.value = resultBox.value + ('\n');//Adding linebreaks for easy reading.
-    }//End of values exist confirmation.
+        resultBox.value = resultBox.value + ('\n');//Ease of reading
+    
 
     //display only 3 or all 6 site check options.
-    if (situationBox.value != '' && pasteBox.value != ''){//Confirm that values exist
-        if (defaultDomainValue == primaryDomainValue && defaultDomainValue != '') {
-            
-            //If data is present we will allow the slack button to be clicked.
-            ToggleSubmitToSlackButton();
-            CleanSlackPosting();//To fill slack content box for issue tracker template
+        if(NoEmptyValuesExist()){ //Checks for empty domain fields
+            if(SubmissionEnabler(resultArray)){ //Submission Enabler function based on array values
+                if (defaultDomainValue === primaryDomainValue) { //Do default and primary match?
 
-            if(defaultDomainValue != ''){//If to prevent strange results after reset.
-                //Perform 3 site checks on default only.
-                AutoApiChecker(defaultDomainValue + '/__mwp2_check__');
-                AutoApiChecker(defaultDomainValue + '/__mwp2_httpd_check__');
-                AutoApiChecker(defaultDomainValue + '/__mwp2_php_check__');
-            }//Preventing strange results after reset. end
-        } else if (defaultDomainValue != '') {
-        
-            //If data is present we will allow the slack button to be clicked.
-            ToggleSubmitToSlackButton();
-            CleanSlackPosting();//To fill slack content box for issue tracker template
-
-            if(defaultDomainValue != '' && primaryDomainValue != ''){//If to prevent strange results    after reset.
-                //Perform all 6 site checks.
-                AutoApiChecker(defaultDomainValue + '/__mwp2_check__');
-                AutoApiChecker(defaultDomainValue + '/__mwp2_httpd_check__');
-                AutoApiChecker(defaultDomainValue + '/__mwp2_php_check__');
-                AutoApiChecker(primaryDomainValue + '/__mwp2_check__');
-                AutoApiChecker(primaryDomainValue + '/__mwp2_httpd_check__');
-                AutoApiChecker(primaryDomainValue + '/__mwp2_php_check__');
-               
+                    DefaultDomainOnly(defaultDomainValue);
+                    parseButton.setAttribute('disabled', 'disabled');//Must disable after  content checks to prevent spam.
+                } else { //Do default and primary match?
+                    DefaulyAndPrimaryDomains(defaultDomainValue, primaryDomainValue);
+                    parseButton.setAttribute('disabled', 'disabled');//Must disable after  content checks to prevent spam.
+                }
+            } else { //Submission Enabler function based on array values
+                console.log(resultArray);
+                PartialReset();
                 
-            }//Preventing strange results after reset. end
-        }//Close of else if statment for default domain value
-        parseButton.setAttribute('disabled', 'disabled');//Must disable after content checks to prevent spam.
-    }//End of if statement to confirm values are properly entered.
-
-
-
+            }  
+        }//Checks for empty domain fields.
+    }//Checks for empty form fields.
 });//End of button click event.
+
+
+/* If a parse is allowed and all values are not present then we will do a 
+- PartialReset. By partially resetting we avoid having arrays containing
+ more then a single set of parsed data. */
+function PartialReset(){
+    //Results Box
+    resultBox.value = 'Text-To-Parse Missing Critical Values. See Console.log for more detail';
+    //Redeclare empty parsing arrays
+    originArray = []; // Global Origin Array will have values set after parse button click.
+    resultArray = []; // Result array will contain escalation details. 
+    
+    //Reset Slack arrays
+    insensitiveSlackArray = [];
+    sensitiveSlackArray = [];
+}
+
+/* Function checks to make sure that both a paste from Agent Toolkit
+as well as an explaination of the situation are present in the form. */
+function SafeToPrintResultsBox() {
+    if(situationBox.value != '' && pasteBox.value != ''){
+        return true;
+    }
+}
+
+/* DefaultDomainOnly is a function which is passed the default domain value.
+Function will then run AutoApiChecker on the necessary domains with pre-set suffix.
+Aslo toggling and creating the slack array within this function. */
+function DefaultDomainOnly(defaultDomainValue){
+    ToggleSubmitToSlackButton();
+    CleanSlackPosting();//To fill slack content box for issue tracker template
+    AutoApiChecker(defaultDomainValue + '/__mwp2_check__');
+    AutoApiChecker(defaultDomainValue + '/__mwp2_httpd_check__');
+    AutoApiChecker(defaultDomainValue + '/__mwp2_php_check__');
+}
+
+/* DefaultAndPrimaryDomains is a function which is passed the two domain values.
+Function will then run AutoApiChecker on the necessary domains with pre-set suffix.
+Aslo toggling and creating the slack array within this function. */
+function DefaulyAndPrimaryDomains(defaultDomainValue, primaryDomainValue){
+    ToggleSubmitToSlackButton();
+    CleanSlackPosting();//To fill slack content box for issue tracker template
+    AutoApiChecker(defaultDomainValue + '/__mwp2_check__');
+    AutoApiChecker(defaultDomainValue + '/__mwp2_httpd_check__');
+    AutoApiChecker(defaultDomainValue + '/__mwp2_php_check__');
+    AutoApiChecker(primaryDomainValue + '/__mwp2_check__');
+    AutoApiChecker(primaryDomainValue + '/__mwp2_httpd_check__');
+    AutoApiChecker(primaryDomainValue + '/__mwp2_php_check__');
+}
+
+/* NoEmpryValuesExist is passed the default and primary domain names
+and returns true if both are not empty values. */
+function NoEmptyValuesExist(Def, Pri){
+    if(Def != '' && Pri != ''){
+        return true;
+    }
+}
+
+/* SubmissionEnabler will take the resultsArray and check it for 
+Mandatory values before allowing submission to Slack. */
+function SubmissionEnabler(array){
+    let x = 0; //x should equal 5 if it has the situation.
+    array.forEach(function(element,index,arr){
+        if (element.match(/Site ID/i) !== null) {x++;}
+        if (element.match(/Customer Number/) !== null) {x++;}
+        if (element.match(/Pimary Domain/) !== null) {x++;}
+        if (element.match(/Default Domain/) !== null) {x++;}
+        if (element.match(/Situation/) !== null) {x++;}
+    })
+    if (x === 4){
+        return true;
+    } else {
+        return false;
+    }
+}
 
 //After existing content checks, this function will remove the hidden display tags and apply inline display.
 function ToggleSubmitToSlackButton(){
@@ -113,8 +176,7 @@ function ToggleSubmitToSlackButton(){
 
 //When reset button is clicked we should reset form to empty. 
 //Elements to clear:
-//PasteBox - ResultBox - situationBox.
-//Reset arrays and DefaultDomain and PrimaryDomain variables.
+//PasteBox - ResultBox - situationBox - Reset arrays
 resetButton.addEventListener('click', function() {
     resetFormValues();
 });
@@ -139,12 +201,12 @@ function CleanSlackPosting(){
             arr[index] = arr[index].replace(':',': \`');
             insensitiveSlackArray.push(arr[index] + ' \`\n');
         }
-        if (element.match(/Request: [^]+/i)){
-            arr[index] = arr[index].replace('Request:','Situation: \`');
+        if (element.match(/Situation: [^]+/i)){
+            arr[index] = arr[index].replace(':',': \`');
             insensitiveSlackArray.push(arr[index] + ' \`\n');
         }
     });
-
+    //Print values for confirmation before submssion to slack.
     insensitiveSlackArray.forEach(function(element,index,arr){
         slackTextBox.value = slackTextBox.value += arr[index];
     });
@@ -224,5 +286,5 @@ slackSubmitButton.addEventListener( "click", function() {
     });  
     slackSubmitButton.setAttribute('disabled', 'disabled');
 });  
-
-
+// For slack configuration please view the API documentation for Incoming Webhooks
+// https://godaddy.slack.com/apps/A0F7XDUAZ-incoming-webhooks?page=1
